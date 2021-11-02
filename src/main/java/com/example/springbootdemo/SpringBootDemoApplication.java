069d2a3a673cb2305a89d155ce3d2b1701fef237
Niclaus233 <1533674625@qq.com>
2021-11-02 22:56:24 +0800

    提交看看会不会消除红色文件名

---
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+    /**
+     * Hello，World
+     *
+     * @param who 参数，非必须
+     * @return Hello, ${who}
+     */
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+        if (who.isBlank()) {
+            who = "World";
+        }
+        return "Hello, {}!"+ who;
+    }
+}
